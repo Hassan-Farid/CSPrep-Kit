955ddde928a2dbb1b9e314d1fa18664f9fbb2a92
Hassan Farid <hassan.far.661@gmail.com>
2022-03-20 22:04:43 +0500

    Frontend for App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
+import { useState } from 'react'
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // new line start
+    const [questionData, setQuestionData] = useState(null)
+
+    function getData() {
+        axios({
+                method: "GET",
+                url: "/questions",
+            })
+            .then((response) => {
+                const res = response.data
+                setQuestionData(({
+                    name: res.name,
+                    link: res.link
+                }))
+            }).catch((error) => {
+                if (error.response) {
+                    console.log(error.response)
+                    console.log(error.response.status)
+                    console.log(error.response.headers)
+                }
+            })
+    }
+    //end of new line 
+
+    return ( <
+        div className = "App" >
+        <
+        div className = "App-header" >
+        <
+        h1 className = "App-title" > CS Prep Kit < /h1> <
+        p className = "App-desc" > Start your preparation via a Single Click < /p> <
+        button className = "App-btn"
+        onClick = { getData } > Generate Question < /button> <
+        /div> <
+        div className = 'App-content' > {
+            questionData && < div >
+            <
+            div className = 'ques-container' >
+            <
+            p className = 'ques-content' > { questionData.name } < /p> <
+            /div> <
+            div className = 'ques-btn' >
+            <
+            a className = 'App-btn'
+            href = { questionData.link }
+            target = "_blank" > View Answer < /a> <
+            /div> <
+            /div>
+        } <
+        /div> <
+        /div>
+    );
 }
 
 export default App;
